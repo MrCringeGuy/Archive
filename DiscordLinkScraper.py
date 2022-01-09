0491a8d87a7a495c09afba81749b7beb1b05931c
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2022-01-09 21:45:22 +0000

    Update DiscordLinkScraper.py

--- DiscordLinkScraper.py
+++ DiscordLinkScraper.py
@@ -1,4 +1,4 @@
-#Crappy idea for an image scraper thingy
+# crappy idea for an image scraper thingy
 
 # try opening url based on https://cdn.discordapp.com/attachments/914687440315371603/929791144878034944/PXL_20220109_173733852.jpg
 # except error of some sort then print nothing
@@ -11,7 +11,7 @@ amt = int(input('[*] How many links to try: '))
 for n in amt:
     print(f'[{i + 1}]')
     try:
-        request(f'[{i + 1}]     {url}')
+        request(f'[{i + 1}]     {url}')     # this is probably done wrong
         print(f'[+] {url}')
-    except SomethingError:
+    except SomethingError:      # i think it's ValueError
         print('[{i + 1}]     [-]')
