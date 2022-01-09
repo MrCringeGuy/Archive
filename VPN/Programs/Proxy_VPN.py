edf848cc7e866f82ac4ba1d4ba2ec16f7f443f03
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2022-01-09 22:47:34 +0000

    Currently testing proxies. (ln69-91)

--- VPN/Programs/Proxy_VPN.py
+++ VPN/Programs/Proxy_VPN.py
@@ -1,5 +1,6 @@
 import time
 from urllib.request import Request, urlopen
+import requests
 
 # using proxies to make a vpn
 
@@ -25,7 +26,7 @@ def connect_location():
     global loca
 
     print('[1] France\n[2] Poland\n[3] Thailand\n[4] Hong Kong\n[5] Canada\n')
-    loca = str(input('[+]Enter the number of your choice: '))
+    loca = str(input('[+] Enter the number of your choice: '))
     
     try:
         proxy_ip_set()
@@ -36,21 +37,28 @@ def connect_location():
 
 def proxy_ip_set():
     global loca
+    global proxy
+    global port
 
     if loca == '1':
-        proxy = {'https': 'https://159.8.114.27:25'}     # France
+        proxy = 'https://159.8.114.27
+        port = '25'     # France
         print(f'[+] Connecting to {proxy} (France)...')
     elif loca == '2':
         proxy = '217.97.101.134'    # Poland
+        port = ''
         print(f'[+] Connecting to {proxy} (Poland)...')
     elif loca == '3':
-        proxy = {'https': 'https://1.0.170.50:4885'}    # Thailand
+        proxy = 'https://1.0.170.50
+        port = '4485'    # Thailand
         print(f'[+] Connecting to {proxy} (Thailand)...')
     elif loca == '4':
-        proxy = {'https': '1.0.170.50:4485'}    # Hong Kong
+        proxy = '1.0.170.50'
+        port = '4485'    # Hong Kong
         print(f'[+] Connecting to {proxy} (Hong Kong)...')
     elif loca == '5':
-        proxy == {'https': '144.217.240.185:9300'}    # Canada
+        proxy = '144.217.240.185'
+        port = '9300'    # Canada
         print(f'[+] Connectiing to {proxy} (Canada)...')
     else:
         print('[-] The number you entered is invalid.\n\nExiting...')
@@ -58,9 +66,10 @@ def proxy_ip_set():
 
 def test():
     global proxy
+    global proxies
 
     print(f'Proxy IP: {proxy}.\nChecking IP...')
-    IP = urlopen(Request('https://api.ipify.org/')).read().decode()
+    IP = requests.get('https://api.ipify.org/', proxies = proxies)
     print(f'Your IP address is {IP}.')
 
     if not IP == proxy:
@@ -70,10 +79,16 @@ def test():
 
 def proxy_connect():
     global proxy
+    global port
+    global proxies
     
     print(proxy)
 
-    # connect to the proxy (I have no clue how :c)
+    proxies = {
+        'https': f'https://{proxy}:{port}'
+    }
+
+    test()
 
 if __name__ == "__main__":
     main()
